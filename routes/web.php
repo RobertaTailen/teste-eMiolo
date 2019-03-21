8887f2c6c9352a94c96a062be86ed63e74c7ab80
Roberta Tailen <robertatailen1@gmail.com>
2019-03-20 22:06:24 -0300

    Cria rotas de login que utilizam o google

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('login/google', 'Auth\LoginController@redirectToProvider')->name('googleLogin');
+Route::get('login/google/callback', 'Auth\LoginController@handleProviderCallback');
